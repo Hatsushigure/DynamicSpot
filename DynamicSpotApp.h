fa4911d37bbf2752f5f51d57f58071e62454daaa
初時雨 <Hatsushigure_c@163.com>
2023-03-19 14:01:14 +0800

    finished ver 0.1.0

--- DynamicSpotApp.h
+++ DynamicSpotApp.h
@@ -2,12 +2,21 @@
 
 class DynamicSpotApp : public QApplication
 {
+	Q_OBJECT
 private:
+	bool m_allReady;
+	QTimer* m_timersplashScreen;
 public:
 	DynamicSpotApp(int argc, char *argv[]);
 	~DynamicSpotApp();
 private:
+	void initSplashScreeen();
+	void initIcons();
 	void initMainWindow();
 	void initTrayMenu();
 	void initTrayIcon();
+private:
+	void removeSplashScreen();
+signals:
+	void allReady();
 };
