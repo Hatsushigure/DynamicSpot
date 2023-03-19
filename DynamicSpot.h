786da75feaa6c1cb3b92fc9e45902a356ed6a61d
初時雨 <Hatsushigure_c@163.com>
2023-03-19 12:07:25 +0800

    添加了托盘图标

--- DynamicSpot.h
+++ DynamicSpot.h
@@ -1,8 +1,12 @@
 class DynamicSpotApp;
 class MyMainWindow;
+class QSystemTrayIcon;
+class QMenu;
 
 namespace DynamicSpot
 {
 extern DynamicSpotApp* theApp;
 extern MyMainWindow* mainWindow;
+extern QMenu* trayMenu;
+extern QSystemTrayIcon* trayIcon;
 }
