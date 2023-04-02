1c0e696fbda827e0580acf0501381528e38ef5c9
初時雨 <Hatsushigure_c@163.com>
2023-04-02 09:31:34 +0800

    将日期计算的控制权交给 C++

--- MyMainWindow.h
+++ MyMainWindow.h
@@ -1,17 +1,26 @@
 #include <QQuickWidget>
+#include <QDate>
 
 class MyMainWindow : public QQuickWidget
 {
+	Q_OBJECT
+	Q_PROPERTY(int daysLeft READ daysLeft WRITE setDaysLeft NOTIFY daysLeftChanged)
 private:
 	const QString uiSource = "qrc:/DynamicSpot/MyMainWindow.qml";
+	const QDate deadline = QDate(2023, 6,7);
 private:
 	QQuickItem* m_rootItem;
 	QQuickItem* m_container;
 	QQuickItem* m_timeBanner;
 	QQuickItem* m_countDown;
 	QTimer* m_timerAdjustColor;
+	QTimer* m_timerUpdateDate;
+	int m_daysLeft;
 public:
 	explicit MyMainWindow(QWidget *parent = nullptr);
+public:
+	int daysLeft() const;
+	void setDaysLeft(int newDaysLeft);
 private:
 	void initView();
 	void initItem();
@@ -19,5 +28,9 @@ private:
 public slots:
 	void adjustGeometry();
 	void adjustcolor();
+	void updatedaysLeft();
+	void updateCountDownText();
 	void startBackgroundTest();
+signals:
+	void daysLeftChanged();
 };
