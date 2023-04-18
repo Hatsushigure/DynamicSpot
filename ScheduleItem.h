dc46dcba4001869bcc27777c9d399e2693efbefa
初時雨 <Hatsushigure_c@163.com>
2023-04-18 13:40:21 +0800

    修改了部分资源路径

--- ScheduleItem.h
+++ ScheduleItem.h
@@ -16,7 +16,7 @@ private:
 	QTime m_time {};
 	QString m_title {"No Title"};
 	QString m_subtitle {""};
-	QString m_iconFileName {"qrc:/DynamicSpot/images/icons/info/info-256.png"};
+	QString m_iconFileName {"qrc:/DynamicSpot/images/icons/colored/info-256.png"};
 	int m_durationSeconds {10};
 	QString m_commandLine {""};
 
