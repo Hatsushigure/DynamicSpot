6492e59b75e82c144d0f1a923345a4361e5e4051
初時雨 <Hatsushigure_c@163.com>
2023-05-29 16:00:06 +0800

    just for sync

--- TimeBanner.h
+++ TimeBanner.h
@@ -13,19 +13,35 @@ class TimeBanner : public QObject
 	Q_PROPERTY(ScheduleHost* scheduleHost READ scheduleHost CONSTANT)
 	Q_PROPERTY(SloganProvider* sloganProvider READ sloganProvider CONSTANT)
 	Q_PROPERTY(QString timeText READ timeText WRITE setTimeText NOTIFY timeTextChanged)
+	Q_PROPERTY(QString stateString READ stateString NOTIFY stateStringChanged CONSTANT);
+public:
+	enum class States {ShowSlogan, ShowSchedule, ShowTime};
+private:
+	const int heartBeeatInterval {500};
+	const int sloganDuration {60 * 1000};
 private:
 	SloganProvider* m_sloganProvider {nullptr};
 	QString m_timeText {""};
-	QTimer* m_timerChangeTime {nullptr};
+	QTimer* m_timerHeartBeat {nullptr};
+	QTimer* m_timerSloganDuration {nullptr};
+	States m_state {States::ShowTime};
+	QString m_stateString {"showTime"};
 public:
 	explicit TimeBanner(QObject *parent = nullptr);
 public:
-	ScheduleHost* scheduleHost() const {return ScheduleHost::instance();}
-	SloganProvider* sloganProvider() const {return m_sloganProvider;}
+	auto scheduleHost() const {return ScheduleHost::instance();}
+	auto sloganProvider() const {return m_sloganProvider;}
 	void setTimeText(const QString& newTimeText);
-	QString timeText() const {return m_timeText;}
+	auto timeText() const {return m_timeText;}
+	auto stateString() const {return m_stateString;}
+	void setStateString(const States newState);
 private slots:
 	void updateTime();
+public slots:
+	void showSlogan();
+	void showTime();
+	void showSchedule();
 signals:
 	void timeTextChanged();
+	void stateStringChanged();
 };
